92d6903cc4ed8cb1750b53236ee6b338140728f5
Peggy Li <xpeggyxli@gmail.com>
2018-11-10 18:06:03 -0500

    add user registration

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(64))
     last_name = db.Column(db.String(64))
@@ -9,3 +11,13 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.first_name)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
