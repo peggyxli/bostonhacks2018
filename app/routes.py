8d973a9c4e56de4d58463512461b6f67f9f1283b
Peggy Li <xpeggyxli@gmail.com>
2018-11-10 23:49:47 -0500

    Add alarm route

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+import requests
 
 @app.route('/')
 @app.route('/index')
@@ -20,6 +21,7 @@ def index():
         }
     ]
     return render_template("index.html", title='Home Page', posts=posts)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -38,10 +40,12 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -56,3 +60,10 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/alarm', methods=['GET', 'POST'])
+def alarm():
+    xml = """<play_info><app_key>fhlvYKfsanEBDRGpFmezAM2iRM7ZuHA7</app_key><url>http://www.music.helsinki.fi/tmt/opetus/uusmedia/esim/a2002011001-e02-128k.mp3</url><service>service text</service><reason>reason text</reason><message>message text</message><volume>25</volume></play_info>"""
+    headers = {'Content-Type': 'application/xml'} # set what your server accepts
+    return requests.post('http://10.192.199.251:8090/speaker', data=xml, headers=headers).text
